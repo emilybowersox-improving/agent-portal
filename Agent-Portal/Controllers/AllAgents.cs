82c0c1bc6530bd44e4e7eeeba8f2fc66d01bcf1d
emilybowersox <emilygbowersox@gmail.com>
2021-06-06 22:10:56 -0500

    url routing

---
+++ Agent-Portal/Controllers/AllAgents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agent_Portal.Controllers
+{
+    public class AllAgents : Controller
+    {
+   /*     public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        // GET /AllAgents/
+        public string Index()
+        {
+            return "here's all the agents";
+        }
+
+        // GET /AllAgents/Welcome
+        public string Welcome(string agentName, int agentNumber = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hi {agentName} your agent number is {agentNumber}"); 
+        }
+    }
+}
---
+++ Agent-Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Agent_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
