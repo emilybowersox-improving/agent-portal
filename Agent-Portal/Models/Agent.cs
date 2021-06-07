9d8abc89f725f7276d6811ab8e9d881aaa5ecd4b
emilybowersox <emilygbowersox@gmail.com>
2021-06-06 23:04:53 -0500

    agent model and installed nuget package

---
+++ Agent-Portal/Models/Agent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agent_Portal.Models
+{
+    public class Agent
+    {
+        public string AgentCode { get; set; }
+        public string AgentName { get; set; }
+        public string WorkingArea { get; set; }
+        public Decimal Commission { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
